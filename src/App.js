a70139fd4cb9a36ba36d9cb7bf3b62700c4adc9f
harianchery <hari.anchery001@gmail.com>
2024-06-11 00:21:30 +0530

    Navbar created and routed

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import AddEmp from './Components/AddEmp';
 import SearchEmp from './Components/SearchEmp';
 import DeleteEmp from './Components/DeleteEmp';
 import ViewAll from './Components/ViewAll';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <AddEmp/>
-      <SearchEmp/>
-      <DeleteEmp/>
-      <ViewAll/>
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<AddEmp/>}/>
+    <Route path='/search' element={<SearchEmp/>}/>
+    <Route path='/delete' element={<DeleteEmp/>}/>
+    <Route path='/view' element={<ViewAll/>}/>
+   </Routes>
+   </BrowserRouter>
   );
 }
 
--- src/Components/SearchEmp.jsx
+++ src/Components/SearchEmp.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Navbar from './Navbar'
 
 const SearchEmp = () => {
     const[emp,changeEmp]=useState(
@@ -16,6 +17,7 @@ const SearchEmp = () => {
     }
   return (
     <div>
+        <Navbar/>
         <div className="container">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
